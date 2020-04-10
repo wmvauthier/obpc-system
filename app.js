d769b60e9ffb8e169bf9c845f98d453fb20cf63a
Wesley Matheus Vauthier <wesley.vauthier@interne.com.br>
2020-04-10 20:05:22 -0300

    C

--- app.js
+++ app.js
@@ -33,11 +33,12 @@ app.use('/usuarios', rotaUsuarios);
 
 app.use((req, res, next) => {
     const erro = new Error('Não encontrado');
-    erro.status(404);
+    erro.status = 404;
     next(erro);
 });
 
 app.use((error, req, res, next) => {
+    res.status(error.status || 500);
     return res.send({
         erro: {
             mensagem: error.message
