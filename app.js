a5bf68d7a2bea46f06c37694c96b92effe334e51
Wesley Matheus Vauthier <wesley.vauthier@interne.com.br>
2020-04-11 23:17:35 -0300

    A

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const rotaIgrejas = require('./routes/igrejas');
 const rotaUsuarios = require('./routes/usuarios');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
