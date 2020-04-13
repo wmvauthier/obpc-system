11e11c1b9982d0b47cc22c4d2b5ce05265dd5b45
Wesley Matheus Vauthier <wesley.vauthier@interne.com.br>
2020-04-13 08:38:29 -0300

    Teste CORS

--- app.js
+++ app.js
@@ -12,16 +12,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin,  X-Requested-With', 'Content-Type', 'Accept', 'Authorization'
-    );
 
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
-        return res.status(200).send({});
-    }
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
 
     next();
 
