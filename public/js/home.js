7f8d51f78e3f3616089afcacfb2a7e9ee080eef4
Wesley Matheus Vauthier <wesley.vauthier@interne.com.br>
2020-04-30 21:24:53 -0300

    Correção das Refatorações

--- public/js/home.js
+++ public/js/home.js
@@ -12,9 +12,7 @@ $(document).ready(function () {
 
 function createChurch(igreja) {
 
-    // var pastor = httpGet(`/pessoas/api/${igreja.pastor}`).pessoas[0].nome;
-    var pastor = httpGet(`/pessoas/api/${igreja.pastor}`);
-    console.log(pastor);
+    var pastor = httpGet(`/pessoas/api/${igreja.pastor}`).pessoas[0].nome;
     $("#resumeMultiCarousel-inner").append($(`
 
     <div class="item">
