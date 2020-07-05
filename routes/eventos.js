5882ef86f5f2f3fdbc63f8c68deb9ca112aee5d6
Wesley Matheus Vauthier <wesley.vauthier@interne.com.br>
2020-07-04 21:45:26 -0300

    A

--- routes/eventos.js
+++ routes/eventos.js
@@ -7,8 +7,8 @@ router.get('/', login.opcional, (req, res, next) => { res.render('igrejas.ejs');
 router.get('/api', login.opcional, EventosController.getAllEventos);
 router.get('/api/listaPersona/:idEvento', login.opcional, EventosController.getListaPersona);
 router.get('/api/listaTicket/:idEvento', login.opcional, EventosController.getListaTickets);
-router.get('/api/insertEvent', login.opcional, EventosController.insertEvent);
-router.get('/api/insertTicket', login.opcional, EventosController.insertTicket);
-router.get('/api/insertTicketPersona', login.opcional, EventosController.insertTicketPersona);
+router.get('/api/insertEvent/:typeEvento/:dataEvento/:vagasEvento', login.opcional, EventosController.insertEvent);
+router.get('/api/insertTicket/:id_evento/:ticket/:personaData', login.opcional, EventosController.insertTicket);
+router.get('/api/insertTicketPersona/:id_evento/:nome/:rg/:status', login.opcional, EventosController.insertTicketPersona);
 
 module.exports = router;
