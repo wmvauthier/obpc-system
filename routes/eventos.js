124e6f9f4ee3008c62aa5247253fb8e90d08c1a9
Wesley Matheus Vauthier <wesley.vauthier@interne.com.br>
2020-07-16 20:16:17 -0300

    Qtd de Vagas

--- routes/eventos.js
+++ routes/eventos.js
@@ -7,6 +7,7 @@ router.get('/', login.opcional, (req, res, next) => { res.render('igrejas.ejs');
 router.get('/api', login.opcional, EventosController.getAllEventos);
 router.get('/api/listaPersona/:idEvento', login.opcional, EventosController.getListaPersona);
 router.get('/api/listaTicket/:idEvento', login.opcional, EventosController.getListaTickets);
+router.get('/api/getQtdTicketsFromEvento/:idEvento', login.opcional, EventosController.getQtdTicketsFromEvento);
 router.get('/api/insertEvent/:typeEvento/:dataEvento/:vagasEvento', login.opcional, EventosController.insertEvent);
 router.get('/api/insertTicket/:id_evento/:ticket/:personaData', login.opcional, EventosController.insertTicket);
 router.get('/api/insertTicketPersona/:id_evento/:nome/:rg/:status', login.opcional, EventosController.insertTicketPersona);
